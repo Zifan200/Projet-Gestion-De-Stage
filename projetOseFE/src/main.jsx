08d5dbae49259187f9494270501a2bd0d7aab121
victoria-fp <2135480@claurendeau.qc.ca>
2025-09-18 19:02:32 -0400

    EQ3-16: début du formulaire de connexion avec validation sur les champs

--- projetOseFE/src/main.jsx
+++ projetOseFE/src/main.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import EtudiantForm from "./components/EtudiantForm.jsx";
+import ConnectionForm from "./components/ConnectionForm.jsx";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <EtudiantForm />
+        {/*<EtudiantForm />*/}
+        <ConnectionForm />
     </React.StrictMode>
 );
