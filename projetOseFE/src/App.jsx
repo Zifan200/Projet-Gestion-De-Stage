4c38e3a942860bbfbce87e1479069d3992e49521
mthibeaultthuot <mathieu.thibeault-thuot@outlook.com>
2025-10-01 18:19:05 -0400

    030-189-FE : refactor : app.jsx page vers des pages et non component

--- projetOseFE/src/App.jsx
+++ projetOseFE/src/App.jsx
@@ -1,45 +1,43 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/menu/Navbar.jsx";
-import ConnectionForm from "./components/ConnectionForm.jsx";
 
-// Pages Étudiant
-import EtudiantForm from "./components/EtudiantForm.jsx";
-import EtudiantConnection from "./components/EtudiantConnection.jsx";
+import { LoginPage } from "./pages/login.jsx";
 
 // Pages Employeur
 import { EmployerSignUpPage } from "./pages/employer/signUp.jsx";
-import { EmployerLoginPage } from "./pages/employer/login.jsx";
 import AccueilEmployer from "./pages/employer/accueilEmployer.jsx";
 import AjoutStage from "./pages/employer/ajoutStage.jsx";
 import { ResetPasswordPage } from "./pages/auth/resetPassword.jsx";
 import { RequestPassword } from "./pages/auth/requestPassword.jsx";
 import { StudentDashboard } from "./pages/dashboard/studentDashboard.jsx";
+import { StudentSignUpPage } from "./pages/student/signUp.jsx";
 import Home from "./pages/home.jsx";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        {/* Pages principales */}
-        <Route path="/" element={<Home />} />
-        <Route path="/reset-password" element={<ResetPasswordPage />} />
-        <Route path="/request-password" element={<RequestPassword />} />
+    <div className="min-h-screen">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          {/* Pages principales */}
+          <Route path="/" element={<Home />} />
+          <Route path="/reset-password" element={<ResetPasswordPage />} />
+          <Route path="/request-password" element={<RequestPassword />} />
 
-        {/* Routes Étudiant */}
-        <Route path="/signup/etudiant" element={<EtudiantForm />} />
+          {/* Routes Étudiant */}
+          <Route path="/signup/student" element={<StudentSignUpPage />} />
 
-        <Route path="/dashboard/student" element={<StudentDashboard />} />
+          <Route path="/dashboard/student" element={<StudentDashboard />} />
 
-        {/* Routes Employeur */}
-        <Route path="/signup/employer" element={<EmployerSignUpPage />} />
-        <Route path="/login" element={<ConnectionForm />} />
-        <Route path="/login/employer" element={<EmployerLoginPage />} />
-        <Route path="/employer/accueil" element={<AccueilEmployer />} />
-        <Route path="/employer/ajout_stages" element={<AjoutStage />} />
-      </Routes>
-    </BrowserRouter>
+          {/* Routes Employeur */}
+          <Route path="/signup/employer" element={<EmployerSignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/employer/accueil" element={<AccueilEmployer />} />
+          <Route path="/employer/ajout_stages" element={<AjoutStage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
