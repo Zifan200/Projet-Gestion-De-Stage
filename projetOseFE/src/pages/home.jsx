5937ca110d4431c9faadeaca0763b9be525d8627
mthibeaultthuot <mathieu.thibeault-thuot@outlook.com>
2025-09-30 21:24:09 -0400

    030-190-FE -> add : basic setup pour i18n (en, fr)

--- projetOseFE/src/pages/home.jsx
+++ projetOseFE/src/pages/home.jsx
@@ -1,5 +1,19 @@
-function Home() {
-    return <>Home</>
-}
+import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
-export default Home();
+export default function Home() {
+  const { t, i18n } = useTranslation();
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <h1>{t("Welcome to reacrt")}</h1>
+    </div>
+  );
+}
