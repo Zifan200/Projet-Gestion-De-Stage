afc08713edd325f165711d522c3c30651b43986c
mthibeaultthuot <mathieu@thibeault.cloud>
2025-10-05 13:45:35 -0400

    030-189-FE -> fix : i18n

--- projetOseFE/src/i18n.js
+++ projetOseFE/src/i18n.js
@@ -5,7 +5,6 @@ import { initReactI18next } from "react-i18next";
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
 export const resources = {
-            },
   en: {
     translation: {
       success: {
