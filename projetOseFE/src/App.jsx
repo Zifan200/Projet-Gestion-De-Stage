fb1764a4dffde3efa22ac4fc2bc2de9948c2e6ed
mthibeaultthuot <mathieu@thibeault.cloud>
2025-10-05 13:38:05 -0400

    030-189-FE -> add : config pour protégé les route

--- projetOseFE/src/App.jsx
+++ projetOseFE/src/App.jsx
@@ -6,15 +6,82 @@ import { LoginPage } from "./pages/login.jsx";
 
 // Pages Employeur
 import { EmployerSignUpPage } from "./pages/employer/signUp.jsx";
-import AccueilEmployer from "./pages/employer/accueilEmployer.jsx";
-import AjoutStage from "./pages/employer/ajoutStage.jsx";
+import AjoutStage from "./pages/employer/addIntership.jsx";
 import { ResetPasswordPage } from "./pages/auth/resetPassword.jsx";
 import { RequestPassword } from "./pages/auth/requestPassword.jsx";
-import { StudentDashboard } from "./pages/dashboard/studentDashboard.jsx";
+import { StudentDashboard } from "./pages/student/dashboard.jsx";
 import { StudentSignUpPage } from "./pages/student/signUp.jsx";
 import Home from "./pages/home.jsx";
+import { EmployerDashboard } from "./pages/employer/dashboard.jsx";
+import { DashboardLayout } from "./components/layouts/dashboard.jsx";
+import {
+  BackpackIcon,
+  PersonIcon,
+  CheckIcon,
+  EnvelopeClosedIcon,
+  FileTextIcon,
+  EnvelopeOpenIcon,
+} from "@radix-ui/react-icons";
+import AddIntership from "./pages/employer/addIntership.jsx";
+import { StudentCVs } from "./pages/student/cvs.jsx";
+import { useTranslation } from "react-i18next";
+import { OfferList } from "./pages/employer/offerList.jsx";
 
 function App() {
+  const { t } = useTranslation();
+  const employerDashboardSidebarLinks = [
+    {
+      key: "dashboard",
+      label: t("menu.dashboard"),
+      href: "/dashboard/employer/",
+      icon: BackpackIcon,
+    },
+    {
+      key: "createOffers",
+      label: t("menu.createOffer"),
+      href: "/dashboard/employer/add-intership",
+      icon: EnvelopeClosedIcon,
+    },
+    {
+      key: "seeOffers",
+      label: t("menu.seeOffer"),
+      href: "/dashboard/employer/my-offers",
+      icon: EnvelopeOpenIcon,
+    },
+    {
+      key: "applications",
+      label: t("menu.post"),
+      href: "/employer/applications",
+      icon: PersonIcon,
+    },
+    {
+      key: "students",
+      label: t("menu.student"),
+      href: "/employer/students",
+      icon: CheckIcon,
+    },
+  ];
+
+  const studentDashboardSidebarLinks = [
+    {
+      key: "home",
+      label: "Dashboard",
+      href: "/dashboard/student",
+      icon: BackpackIcon,
+    },
+    {
+      key: "cvs",
+      label: t("menu.cvs"),
+      href: "/dashboard/student/cvs",
+      icon: FileTextIcon,
+    },
+    {
+      key: "applications",
+      label: t("menu.post"),
+      href: "/dashboard/student/applications",
+      icon: EnvelopeClosedIcon,
+    },
+  ];
   return (
     <div className="min-h-screen">
       <BrowserRouter>
@@ -27,13 +94,41 @@ function App() {
 
           {/* Routes Étudiant */}
           <Route path="/signup/student" element={<StudentSignUpPage />} />
+          <Route
+            path="/dashboard/student/"
+            element={
+              <DashboardLayout
+                sidebarLinks={studentDashboardSidebarLinks}
+                title="Student"
+              />
+            }
+          >
+            <Route index element={<StudentDashboard />} />
+            <Route path="/dashboard/student/cvs" element={<StudentCVs />} />
+          </Route>
 
-          <Route path="/dashboard/student" element={<StudentDashboard />} />
-
+          <Route
+            path="/dashboard/employer/"
+            element={
+              <DashboardLayout
+                sidebarLinks={employerDashboardSidebarLinks}
+                title="Employeur"
+              />
+            }
+          >
+            <Route index element={<EmployerDashboard />} />
+            <Route
+              path="/dashboard/employer/add-intership"
+              element={<AddIntership />}
+            />
+            <Route
+              path="/dashboard/employer/my-offers"
+              element={<OfferList />}
+            />
+          </Route>
           {/* Routes Employeur */}
           <Route path="/signup/employer" element={<EmployerSignUpPage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/employer/accueil" element={<AccueilEmployer />} />
           <Route path="/employer/ajout_stages" element={<AjoutStage />} />
         </Routes>
       </BrowserRouter>
