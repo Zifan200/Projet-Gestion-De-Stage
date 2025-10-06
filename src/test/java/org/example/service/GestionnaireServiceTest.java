95861b8d20917bd518a717a3af9d520f3760deca
YahyaMTL <134558863+YahyaMTL@users.noreply.github.com>
2025-10-05 20:56:33 -0400

    Update src/test/java/org/example/service/GestionnaireServiceTest.java
    Co-authored-by: victoria-fp <132675347+victoria-fp@users.noreply.github.com>

--- src/test/java/org/example/service/GestionnaireServiceTest.java
+++ src/test/java/org/example/service/GestionnaireServiceTest.java
@@ -13,8 +13,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import java.time.LocalDate;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 @ExtendWith(MockitoExtension.class)
 class GestionnaireServiceTest {
