cdf29d1d1f6b1b8c9e30e9f6874b2f1b52e04007
ZohnHadley <zohnhadley@gmail.com>
2025-10-10 17:26:56 -0400

    updated: dto's related to InternshipApplication

--- src/main/java/org/example/service/dto/InternshipApplicaiton/InternshipApplicationDto.java
+++ src/main/java/org/example/service/dto/InternshipApplicaiton/InternshipApplicationDto.java
@@ -1,10 +1,62 @@
 package org.example.service.dto.InternshipApplicaiton;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.model.InternshipApplication;
+import org.example.model.enums.ApprovalStatus;
+import org.example.service.dto.InternshipOfferDto;
 
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
 @Getter
 @Data
 public class InternshipApplicationDto {
-    
+    private Long id;
+
+    @NotBlank(message = "required: student email")
+    @NotEmpty
+    private String studentEmail;
+
+    @NotBlank(message = "required: CV id")
+    @NotEmpty
+    private Long cvId;
+
+    @NotBlank(message = "required: internship offer id")
+    @NotEmpty
+    private Long  internshipOfferId;
+
+    @NotEmpty
+    private ApprovalStatus status =  ApprovalStatus.PENDING;
+    @NotEmpty
+    private LocalDateTime createdAt;
+
+    @Builder
+    public InternshipApplicationDto(
+            Long id, String studentEmail, Long cvId, Long internshipOfferId, ApprovalStatus status, LocalDateTime createdAt
+    ){
+        this.id = id;
+        this.studentEmail = studentEmail;
+        this.cvId = cvId;
+        this.internshipOfferId = internshipOfferId;
+        this.status = status;
+        this.createdAt = createdAt;
+    }
+
+    public static InternshipApplicationDto create(InternshipApplication internshipApplication) {
+        return InternshipApplicationDto.builder()
+                .id(internshipApplication.getId())
+                .studentEmail(internshipApplication.getStudent().getEmail())
+                .cvId(internshipApplication.getSelectedStudentCV().getId())
+                .status(internshipApplication.getStatus())
+                .build();
+    }
+
+    public static InternshipApplicationDto empty(){
+        return new InternshipApplicationDto();
+    }
 }
--- src/main/java/org/example/service/dto/InternshipApplicaiton/InternshipApplicationResponseDto.java
+++ src/main/java/org/example/service/dto/InternshipApplicaiton/InternshipApplicationResponseDto.java
@@ -1,10 +1,70 @@
 package org.example.service.dto.InternshipApplicaiton;
 
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.model.InternshipApplication;
+import org.example.model.enums.ApprovalStatus;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
 @Getter
 @Data
 public class InternshipApplicationResponseDto {
+
+    private Long id;
+    private String studentEmail;
+
+    private Long selectedCvID;
+    private String selectedCvFileName;
+    private Long selectedCvFileSize;
+
+    private Long internshipOfferId;
+    private String internshipOfferTitle;
+    private String internshipOfferEmployerEmail;
+    private LocalDate internshipOfferPublishedDate;
+    private LocalDate internshipOfferExpirationDate;
+
+    private ApprovalStatus status =  ApprovalStatus.PENDING;
+    private LocalDateTime createdAt;
+
+    @Builder
+    public InternshipApplicationResponseDto(
+            Long id, String studentEmail, Long selectedCvID, String selectedCvFileName, Long selectedCvFileSize,
+            Long internshipOfferId, String internshipOfferTitle, String internshipOfferEmployerEmail,
+            LocalDate internshipOfferPublishedDate, LocalDate internshipOfferExpirationDate, ApprovalStatus status, LocalDateTime createdAt
+
+            ){
+        this.id = id;
+        this.studentEmail = studentEmail;
+        this.selectedCvID = selectedCvID;
+        this.selectedCvFileName = selectedCvFileName;
+        this.selectedCvFileSize = selectedCvFileSize;
+        this.internshipOfferId = internshipOfferId;
+        this.internshipOfferTitle = internshipOfferTitle;
+        this.internshipOfferEmployerEmail = internshipOfferEmployerEmail;
+        this.internshipOfferPublishedDate = internshipOfferPublishedDate;
+        this.internshipOfferExpirationDate = internshipOfferExpirationDate;
+        this.status = status;
+        this.createdAt = createdAt;
+    }
+
+    public static InternshipApplicationResponseDto create(InternshipApplication internshipApplication){
+        return InternshipApplicationResponseDto.builder()
+                .studentEmail(internshipApplication.getStudent().getEmail())
+                .internshipOfferId(internshipApplication.getOffer().getId())
+                .selectedCvID(internshipApplication.getSelectedStudentCV().getId())
+                .selectedCvFileName(internshipApplication.getSelectedStudentCV().getFileName())
+                .selectedCvFileSize(internshipApplication.getSelectedStudentCV().getFileSize())
+                .internshipOfferId(internshipApplication.getOffer().getId())
+                .internshipOfferTitle(internshipApplication.getOffer().getTitle())
+                .internshipOfferPublishedDate(internshipApplication.getOffer().getPublishedDate())
+                .internshipOfferExpirationDate(internshipApplication.getOffer().getExpirationDate())
+                .createdAt(internshipApplication.getCreatedAt())
+                .build();
+    }
 }
