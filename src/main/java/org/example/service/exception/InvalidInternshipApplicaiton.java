d14dbbe22095e49fb04e802ead9e3adfffad1c02
ZohnHadley <zohnhadley@gmail.com>
2025-10-12 09:23:49 -0400

    refactor: moved and renamed InvalidInternshipApplicaiton.java --> InvalidInternshipApplicationException.java in the exception folder in services

--- src/main/java/org/example/service/exception/InvalidInternshipApplicaiton.java
+++
@@ -1,7 +0,0 @@
-package org.example.service.exception;
-
-public class InvalidInternshipApplicaiton extends RuntimeException {
-    public InvalidInternshipApplicaiton(String message) {
-        super(message);
-    }
-}
