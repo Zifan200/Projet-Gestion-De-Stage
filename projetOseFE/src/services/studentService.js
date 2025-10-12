2246306109818c330bcc29539ca2ed5622c58d89
Zifan Wang <lastrain95000@gmail.com>
2025-10-12 15:52:09 -0400

    feat(frontend): ajout de la fonction applyCV dans studentService pour postuler à une offre

--- projetOseFE/src/services/studentService.js
+++ projetOseFE/src/services/studentService.js
@@ -5,4 +5,13 @@ export const studentService = {
     const res = await api.post("/student/register", student);
     return res.data;
   },
+
+  async applyCV(token, payload) {
+    const res = await api.post("/student/apply-to-internship-offer", payload, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return res.data;
+  },
 };
