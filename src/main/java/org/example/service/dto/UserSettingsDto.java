6663b540cf66d145a3cd7cd9ce05bca2cdb78602
mthibeaultthuot <mathieu@thibeault.cloud>
2025-10-15 14:25:08 -0400

    030-28-128-BE -> add : objet de base pour les settings de l'utilisateur

---
+++ src/main/java/org/example/service/dto/UserSettingsDto.java
@@ -0,0 +1,20 @@
+package org.example.service.dto;
+
+import lombok.*;
+import org.example.model.UserSettings;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserSettingsDto {
+
+    private String language;
+
+    public static UserSettingsDto fromEntity(UserSettings userSettings) {
+        return UserSettingsDto.builder()
+                .language(userSettings.getLanguage())
+                .build();
+    }
+}
