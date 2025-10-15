bd42c4e2e04c93467ebb5d1c09b17ddce94095fb
mthibeaultthuot <mathieu@thibeault.cloud>
2025-10-15 14:26:43 -0400

    030-28-128-BE -> add api response pour enterprise level response pour les controller

--- src/main/java/org/example/service/dto/ApiSuccessResponse.java
+++ src/main/java/org/example/service/dto/ApiSuccessResponse.java
@@ -1,4 +1,26 @@
 package org.example.service.dto;
 
-public class ApiSuccessResponse {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class ApiSuccessResponse<T> {
+    private String code;
+    private String message;
+    private T data;
+    private LocalDateTime timestamp;
+
+    public static <T> ApiSuccessResponse<T> of(String code, String message, T data) {
+        return ApiSuccessResponse.<T>builder()
+                .code(code)
+                .message(message)
+                .data(data)
+                .timestamp(LocalDateTime.now())
+                .build();
+    }
 }
