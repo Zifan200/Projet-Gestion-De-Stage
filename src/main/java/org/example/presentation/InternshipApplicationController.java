fe8db580401fb0aba717df007211e43f335b97c5
ZohnHadley <zohnhadley@gmail.com>
2025-10-16 11:41:26 -0400

    add : InternshipApplicationController

---
+++ src/main/java/org/example/presentation/InternshipApplicationController.java
@@ -0,0 +1,29 @@
+package org.example.presentation;
+
+
+import lombok.RequiredArgsConstructor;
+import org.example.service.InternshipApplicationService;
+import org.example.service.dto.InternshipApplication.InternshipApplicationResponseDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/internship-applicaitons")
+@CrossOrigin(origins = "http://localhost:5173")
+public class InternshipApplicationController {
+
+    private final InternshipApplicationService  internshipApplicationService;
+
+    @GetMapping("/get-all")
+    public ResponseEntity<List<InternshipApplicationResponseDTO>> getAllInternshipApplications(){
+        return ResponseEntity.ok(internshipApplicationService.getAllApplications());
+    }
+
+    @GetMapping("get-all/{status}")
+    public ResponseEntity<List<InternshipApplicationResponseDTO>> getAllInternshipApplications(@PathVariable String status){
+        return ResponseEntity.ok(internshipApplicationService.getAllApplicationWithStatus(status));
+    }
+}
