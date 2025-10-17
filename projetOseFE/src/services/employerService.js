80c3b1eaa7e512703f330183c7a591e1ab8a0e1a
Zifan Wang <lastrain95000@gmail.com>
2025-10-17 18:01:57 -0400

    FE: getAllApplications implémenté dans employerService

--- projetOseFE/src/services/employerService.js
+++ projetOseFE/src/services/employerService.js
@@ -5,4 +5,16 @@ export const employerService = {
     const res = await api.post("/employer/register", employer);
     return res.data;
   },
+
+  async getAllApplications() {
+    const token = localStorage.getItem("jwtToken"); // Récupère le JWT stocké
+    const res = await fetch("http://localhost:8080/api/v1/employer/get-all-internship-applications", {
+      headers: {
+        "Authorization": `Bearer ${token}`,
+        "Content-Type": "application/json"
+      }
+    });
+
+    return await res.json();
+  }
 };
