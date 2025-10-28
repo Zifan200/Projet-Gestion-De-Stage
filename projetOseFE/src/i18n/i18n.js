c96bd2dda4f3523e7ee231d2350bc468712a2f3c
mthibeaultthuot <mathieu@thibeault.cloud>
2025-10-28 18:29:02 -0400

    030-196-203 -> add : language avec les folders

---
+++ projetOseFE/src/i18n/i18n.js
@@ -0,0 +1,40 @@
+import i18n from "i18next";
+import backend from "i18next-http-backend";
+import detector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+
+console.warn("i18n Loading");
+const savedLang = localStorage.getItem("lang") || "fr";
+
+i18n
+  .use(backend)
+  .use(detector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "fr",
+    lng: savedLang,
+    ns: [
+      "common",
+      "menu",
+      "login",
+      "signup_student",
+      "signup_employer",
+      "student_dashboard",
+      "student_dashboard_cvs",
+    ],
+    defaultNS: "common",
+    interpolation: {
+      escapeValue: false,
+    },
+    nsSeparator: ":",
+    detection: {
+      order: ["localStorage", "navigator"],
+      caches: ["localStorage"],
+    },
+  });
+
+i18n.on("languageChanged", (lng) => {
+  localStorage.setItem("lang", lng);
+});
+
+export default i18n;
