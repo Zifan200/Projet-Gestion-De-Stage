4e339903683b8e1424662f2805b0a30e9532e325
ZohnHadley <zohnhadley@gmail.com>
2025-11-09 00:44:05 -0500

    added some more stuff to component tableActionButton.jsx

--- projetOseFE/src/components/ui/tableActionButton.jsx
+++ projetOseFE/src/components/ui/tableActionButton.jsx
@@ -7,14 +7,18 @@ export const TableActionButton = ({
                                       onClick,
                                       bg_color = "gray-200",
                                       text_color = "gray-700",
+                                      className="",
+                                      interactive=true
                                   }) => {
     return (
         <button
-            onClick={onClick}
+            onClick={()=>{if(interactive) {
+                onClick
+            }}}
             className={clsx(
                 `inline-flex items-center justify-center gap-1.5 px-3 py-1.5 rounded-lg text-sm font-medium transition-all duration-200 
-         text-${text_color} bg-${bg_color} hover:bg-current/25 hover:text-${text_color}
-         max-sm:w-9 max-sm:h-9 max-sm:gap-0 min-w-[2.5rem]`,
+         text-${text_color} bg-${bg_color} ${interactive?"hover:bg-current/25":""} hover:text-${text_color}
+         max-sm:w-9 max-sm:h-9 max-sm:gap-0 min-w-[2.5rem] ${className}`,
             )}
         >
             {Icon && (
@@ -26,11 +30,11 @@ export const TableActionButton = ({
                 />
             )}
             {/* Label hidden on small screens */}
-            <span
+            {label ? <span
                 className={`relative z-10 max-sm:opacity-0 max-sm:w-0 max-sm:overflow-hidden transition-all duration-200 whitespace-nowrap`}
             >
-        {label}
-      </span>
+                {label}
+            </span> : <></>}
         </button>
     );
 };
