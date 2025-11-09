14056f53ba5864d234ef02e2955e9954e0e8a406
ZohnHadley <zohnhadley@gmail.com>
2025-11-08 21:41:26 -0500

    changed : Table.jsx modified style a little bit

--- projetOseFE/src/components/ui/table.jsx
+++ projetOseFE/src/components/ui/table.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export const Table = ({ headers, rows, emptyMessage }) => {
   return (
-    <div className="overflow-x-auto bg-white shadow rounded">
-      <table className="w-full text-sm text-left border-collapse">
-        <thead className="bg-[#F9FBFC] text-gray-600 uppercase text-xs font-semibold">
+      <div className="overflow-x-auto bg-white shadow rounded">
+        <table className="w-full text-sm text-left border-collapse">
+          <thead className="bg-[#F9FBFC] text-gray-600 uppercase text-xs font-semibold">
           <tr>
             {headers.map((h) => (
               <th key={h} className="px-4 py-3">
