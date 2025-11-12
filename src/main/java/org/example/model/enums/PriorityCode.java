23c5d5718e3a2519aa5eece5a313f25372da585d
mthibeaultthuot <mathieu@thibeault.cloud>
2025-11-12 14:10:33 -0500

    030-30-83-BE Add code de priorité

---
+++ src/main/java/org/example/model/enums/PriorityCode.java
@@ -0,0 +1,7 @@
+package org.example.model.enums;
+
+public enum PriorityCode {
+    GREEN,      // Recommandé - Offre pertinente
+    BLUE,       // Prioritaire - Excellente correspondance
+    GOLD        // Priorité Maximale - Opportunité exceptionnelle
+}
